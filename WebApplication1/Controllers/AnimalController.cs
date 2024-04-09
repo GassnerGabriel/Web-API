f1ca981cd3482a508ca005c700aa1773cfcfb5ca
LabInfo <LabInfo@TERREOA3M11>
2024-04-09 20:49:20 -0300

    Criando teste Unitario;

---
+++ WebApplication1/Controllers/AnimalController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AnimalController : Controller
+    {
+        List<Animal> lista;
+        public AnimalController()
+        {
+            lista = new List<Animal>();
+            lista.Add(new Animal() {Especie = "Cachorro", Cor = "Caramelo", Genero = false, Idade = 5, Raca = "Vira-Lata"});
+            lista.Add(new Animal() {Especie = "Peixe", Cor = "Listrado", Genero = true, Idade = 6, Raca = "Carpa" });
+            lista.Add(new Animal() { Especie = "Gato", Cor = "Preto", Genero = false, Idade = 4, Raca = "Chaves" });
+            lista.Add(new Animal() { Especie = "Coelho", Cor = "Branco", Genero = true, Idade = 7, Raca = "Pascoa" });
+        }
+        // GET: Animal
+        public ActionResult Index()
+        {
+            return View("Index", lista);
+        }
+
+        // GET: Animal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View("Details", lista[id]);
+        }
+
+        // GET: Animal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- WebApplication1/Scripts/ko/usuario.js
+++ WebApplication1/Scripts/ko/usuario.js
@@ -1,36 +1,25 @@
 ﻿var host = document.location.protocol
     + "//" + document.location.host
-    + ":"(document.location.port
-    + "/api/Usuarios/";
-
+    + ":" + document.location.port
+    + "/api/Usuarios"
 var UsuarioModel = new function () {
     model = this
+    model.email = ko.observable();
+    model.senha = ko.observable();
+    model.confirmarSenha = ko.observable();
+    model.confirmar = ko.computed(function () {
+        return model.senha() == model.confirmarSenha()
+    }, this);
     model.usuarios = ko.observable([
         {
             "Codigo": 1,
             "Email": "fabiano@gmail.com",
             "UltimoAcesso": "2024-03-20T19:13:55.497"
-        }
-    ]);
-    model.email = ko.observable()
-    model.senha = ko.observable()
-    model.confirmarSenha = ko.observable()
-    model.confirmar = ko.computed(function () {
-        return model.senha() == model.confirmarSenha();
-    }, this);
-
-    model.carregar = function () {
-        var settings = {
-            "url": "http://localhost:50751/api/Usuarios",
-            "method": "GET",
-            "timeout": 0,
         };
-
-        $.ajax(settings).done(function (response) {
-            model.usuarios(response);
-        });
-    };
-        model.carregar();
+    $.ajax(settings).done(function (response) {
+        model.usuarios(response);
+    });
+};
+model.carregar();
 }
-
-    ko.applyBindings(UsuarioModel);
+ko.applyBindings(UsuarioModel);
